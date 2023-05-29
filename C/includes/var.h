302e7ddb265f161196af914146316547684178a8
RaphtikGHG <raphael.wegmann81@gmail.com>
2023-05-29 17:25:19 +0200

    Kinda Changed a lot

---
+++ C/includes/var.h
@@ -0,0 +1,6 @@
+//  ##################
+//  #    DEFINES     #
+//  ##################
+
+#define charArrayLimit 1024 //  How many characters can be in an array
+#define cAL 1024 //  How many characters can be in an array
